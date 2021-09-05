be80050d86f463881af395661554c87d48261b7d
David Olea <david@Davids-MacBook-Air.local>
2021-09-04 23:01:24 -0700

    added css js and html search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,23 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response){
+    if(response.ok) {
+      response.json().then(function(data){
+        displayRepos(data.items, language)
+        console.log(data)
+      });
+    }
+    else{
+      alert("Error:Github User not found")
+    }
+  });
+};
 
 var formSumbitHandler = function(event) {
     event.preventDefault();
@@ -83,7 +100,19 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language") 
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+  
+  console.log(language)
+}
 
 
   getUserRepos()
   userFormEl.addEventListener("submit",formSumbitHandler);
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
